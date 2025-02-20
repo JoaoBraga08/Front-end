a84170859256a61faf415ffb277b4f6f9b56e7ea
JoaoBraga08 <joaovitorbaraujo08@gmail.com>
2025-02-20 08:47:10 -0300

    adicionando nova script

---
+++ front-04/variavel.js
@@ -0,0 +1,9 @@
+// alert("olá");
+console.log("Olá braga"); //string
+let idade = 16; //int
+let casado= false; //Booleano
+
+//imprimindo variaveis
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof casado);
---
+++ front-04/variavel2.js
@@ -0,0 +1,5 @@
+const nome = prompt("Digite seu nome");
+const idade = prompt("Digite sua idade");
+
+// console.log("meu nome é: " + nome + " e Minha idade é: " + idade + " Anos")
+console.log(`Meu Nome é: ${nome} e minha idade é: ${idade} Anos`);
