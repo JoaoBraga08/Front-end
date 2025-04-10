16e4f320341338ccdb6547ce09a263b178e9a1ab
JoaoBraga08 <joaovitorbaraujo08@gmail.com>
2025-04-10 10:40:05 -0300

    finalizando aula do dia 10/04

--- front-09/script.js
+++ front-09/script.js
@@ -1,24 +1,59 @@
 const cars = [
     {
         brand: "Toyota",
-        model: "corolla",
-        year: 2020
+        model: "corolla gr",
+        year: 2021
     },
     {
         brand: "Honda",
-        model: "Civic",
-        year: 2019
+        model: "integra type r",
+        year: 1995
     },   
     {
         brand: "Ford",
-        model: "Fiesta",
-        year: 2014
+        model: "gt500",
+        year: 2010
     },
     {
-        brand: "Ford",
-        model: "Focus",
-        year: 2018
+        brand: "Ferrari",
+        model: "f430",
+        year: 2011
+    },
+    {
+        brand: "AlfaRomeu",
+        model: "Giulia",
+        year: 2019
     }
 ];
 
 /* Criando uma Função */
+function displayCars() {
+// Variavel
+const cardList = document.querySelector("#card-list");
+
+cars.forEach((car) =>{
+    console.log(car);
+    // Criar um elemento div pelo JavaScript
+    const cardDiv = document.createElement("div");
+     // Criando uma classe css car
+     cardDiv.classList.add("car");
+
+     // Criando o modelo
+     const cardModel = document.createElement("h2");
+     cardModel.textContent = `${car.brand} ${car.model}`;
+
+     // Criando o Ano 
+     const cardYear = document.createElement("p");
+     cardYear.textContent= `Ano: ${car.year}`;
+
+     cardDiv.appendChild(cardModel);
+     cardDiv.appendChild(cardYear);
+
+     cardList.appendChild(cardDiv);
+
+});
+
+}
+
+// Chamar função 
+displayCars();
