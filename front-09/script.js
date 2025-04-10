9261e22c659a41c4ebdcac16ba082a5c9bac78a0
JoaoBraga08 <joaovitorbaraujo08@gmail.com>
2025-04-10 08:54:08 -0300

    adicionando estrutura html css e js

---
+++ front-09/script.js
@@ -0,0 +1,24 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "corolla",
+        year: 2020
+    },
+    {
+        brand: "Honda",
+        model: "Civic",
+        year: 2019
+    },   
+    {
+        brand: "Ford",
+        model: "Fiesta",
+        year: 2014
+    },
+    {
+        brand: "Ford",
+        model: "Focus",
+        year: 2018
+    }
+];
+
+/* Criando uma Função */
